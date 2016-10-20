6ce31dcad02e372a9ccc5b7b2d2069bacd4fa60a
Mario Martinez <zenitram.oiram@gmail.com>
2016-10-20 17:43:59 +1100

    Circle bounce

--- src/CircularCollision.elm
+++ src/CircularCollision.elm
@@ -1,7 +1,10 @@
 module CircularCollision
     exposing
         ( Circle
+        , Vector
         , collision
+        , velocityAfterCollision
+        , circleAtCollision
         )
 
 
@@ -10,6 +13,10 @@ type alias Circle =
     , cy : Float
     , radius : Float
     }
+
+
+type alias Vector =
+    ( Float, Float )
 
 
 collision : Circle -> Circle -> Bool
@@ -22,9 +29,118 @@ collision circle1 circle2 =
             circle1.cy - circle2.cy
 
         distance =
-            sqrt ((dx * dx) + (dy * dy))
+            hypotenuse dx dy
     in
-        if distance < circle1.radius + circle2.radius then
-            True
-        else
-            False
+        distance < circle1.radius + circle2.radius
+
+
+
+-- velocityAfterCollision formula based on response from:
+-- http://math.stackexchange.com/questions/13261/how-to-get-a-reflection-vector
+
+
+velocityAfterCollision : Circle -> Circle -> Vector -> Vector
+velocityAfterCollision stationaryCircle movingCircle velocity =
+    let
+        mirror =
+            unitMirrorVector stationaryCircle movingCircle
+    in
+        vectorSubtraction velocity (scale (2 * dotProduct velocity mirror) mirror)
+
+
+circleAtCollision : Circle -> Circle -> Vector -> Circle
+circleAtCollision stationaryCircle movingCircle velocity =
+    let
+        adjustmentVector =
+            adjustmentToCollision stationaryCircle movingCircle velocity
+
+        ( collisionPointX, collisionPointY ) =
+            vectorAddition ( movingCircle.cx, movingCircle.cy ) adjustmentVector
+    in
+        Circle collisionPointX collisionPointY movingCircle.radius
+
+
+adjustmentToCollision : Circle -> Circle -> Vector -> Vector
+adjustmentToCollision stationaryCircle movingCircle velocity =
+    let
+        circleCentresX =
+            movingCircle.cx - stationaryCircle.cx
+
+        circleCentresY =
+            movingCircle.cy - stationaryCircle.cy
+
+        circleCentresVector =
+            ( circleCentresX, circleCentresY )
+
+        sumCircleRadii =
+            stationaryCircle.radius + movingCircle.radius
+
+        circleCentresDistance =
+            hypotenuse circleCentresX circleCentresY
+
+        negatedVelocityUnitVector =
+            velocity
+                |> negate
+                |> normalise
+    in
+        scale (2 * (sumCircleRadii - circleCentresDistance)) <|
+            negatedVelocityUnitVector
+
+
+vectorAddition : Vector -> Vector -> Vector
+vectorAddition ( a, b ) ( x, y ) =
+    ( a + x, b + y )
+
+
+vectorSubtraction : Vector -> Vector -> Vector
+vectorSubtraction ( a, b ) ( x, y ) =
+    ( a - x, b - y )
+
+
+dotProduct : Vector -> Vector -> Float
+dotProduct ( a, b ) ( x, y ) =
+    a * x + b * y
+
+
+scale : Float -> Vector -> Vector
+scale s ( x, y ) =
+    ( s * x, s * y )
+
+
+negate : Vector -> Vector
+negate vector =
+    scale -1 vector
+
+
+normalise : Vector -> Vector
+normalise ( a, b ) =
+    let
+        magnitude =
+            hypotenuse a b
+    in
+        ( a / magnitude, b / magnitude )
+
+
+unitMirrorVector : Circle -> Circle -> Vector
+unitMirrorVector stationaryCircle movingCircle =
+    let
+        x =
+            stationaryCircle.cx - movingCircle.cx
+
+        y =
+            stationaryCircle.cy - movingCircle.cy
+
+        magnitude =
+            hypotenuse x y
+    in
+        ( x / magnitude, y / magnitude )
+
+
+square : number -> number
+square n =
+    n ^ 2
+
+
+hypotenuse : Float -> Float -> Float
+hypotenuse a b =
+    sqrt (square a + square b)
--- src/Main.elm
+++ src/Main.elm
@@ -5,7 +5,7 @@ import Html.App as App
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import AnimationFrame
-import CircularCollision as CC exposing (Circle)
+import CircularCollision as CC exposing (Circle, Vector)
 
 
 main : Program Never
@@ -25,12 +25,13 @@ main =
 type alias Model =
     { stationaryCircle : Circle
     , movingCircle : Circle
+    , velocity : Vector
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model (Circle 30 30 20) (Circle 50 0 45)
+    ( Model (Circle 30 100 20) (Circle 50 500 45) ( 0, -2 )
     , Cmd.none
     )
 
@@ -47,15 +48,49 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick newTime ->
-            if model.movingCircle.cy > 200 then
+            if model.movingCircle.cy < (0 - model.movingCircle.radius) then
                 init
             else
-                { model | movingCircle = advanceCircle model.movingCircle } ! []
+                let
+                    colliding =
+                        CC.collision model.stationaryCircle model.movingCircle
+                in
+                    { model
+                        | movingCircle = nextPosition model colliding
+                        , velocity = nextVelocity model colliding
+                    }
+                        ! []
 
 
-advanceCircle : Circle -> Circle
-advanceCircle circle =
-    Circle circle.cx (circle.cy + 1) circle.radius
+nextPosition : Model -> Bool -> Circle
+nextPosition model colliding =
+    if colliding then
+        CC.circleAtCollision model.stationaryCircle model.movingCircle model.velocity
+    else
+        advanceCircle model.movingCircle model.velocity
+
+
+advanceCircle : Circle -> Vector -> Circle
+advanceCircle circle ( x, y ) =
+    Circle (circle.cx + x) (circle.cy + y) circle.radius
+
+
+nextVelocity : Model -> Bool -> Vector
+nextVelocity model colliding =
+    let
+        stationaryCircle =
+            model.stationaryCircle
+
+        movingCircle =
+            model.movingCircle
+
+        velocity =
+            model.velocity
+    in
+        if colliding then
+            CC.velocityAfterCollision stationaryCircle movingCircle velocity
+        else
+            model.velocity
 
 
 
@@ -74,18 +109,9 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     svg [ viewBox "0 0 100 500", width "300px" ]
-        [ movingCircle model.movingCircle
+        [ circleToSvg model.movingCircle "#18a19a"
         , circleToSvg model.stationaryCircle "#0B79CE"
         ]
-
-
-movingCircle : Circle -> Svg a
-movingCircle circle =
-    let
-        circleYPosition =
-            circle.cy * -3 + 500
-    in
-        circleToSvg { circle | cy = circleYPosition } "#89fb5e"
 
 
 circleToSvg : Circle -> String -> Svg a
@@ -100,4 +126,10 @@ circleToSvg circle fillColour =
         radius =
             toString circle.radius
     in
-        Svg.circle [ cx xCentre, cy yCentre, r radius, fill fillColour ] []
+        Svg.circle
+            [ cx xCentre
+            , cy yCentre
+            , r radius
+            , fill fillColour
+            ]
+            []
